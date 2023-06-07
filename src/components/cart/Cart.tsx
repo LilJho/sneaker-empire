7a30f0901f6743da637885fbdc430bf3dfa37d0f
LilJho <jhonnelg25@gmail.com>
2023-06-07 22:56:18 +0800

    added table and used from shadcnui

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -5,6 +5,16 @@ import { useSelector } from "react-redux";
 import { stateType } from "./cartSlice";
 import { removeToCart } from "./cartSlice";
 import { useDispatch } from "react-redux";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableCaption,
+} from "../ui/table";
 
 interface MyComponentProps {
   closeModal: () => void;
@@ -22,12 +32,33 @@ const Cart: React.FC<MyComponentProps> = ({ closeModal }) => {
           <MdOutlineCancel onClick={closeModal} className="cursor-pointer" />
         </div>
 
-        <table className="w-full mt-12 bg-gray-100 rounded-lg">
+        <Table>
+          <TableHeader>
+            <TableRow className="flex items-center justify-between w-full">
+              <TableHead>
+                <input type="checkbox" />
+                <span>Select All</span>
+              </TableHead>
+              <TableHead>Variation</TableHead>
+              <TableHead>Unit Price</TableHead>
+              <TableHead>Quantity</TableHead>
+              <TableHead>Total Price</TableHead>
+              <TableHead>Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {cartItems.map((cartItem: stateType) => (
+              <OrderCard cartItem={cartItem} />
+            ))}
+          </TableBody>
+        </Table>
+
+        {/* <table className="w-full mt-12 bg-gray-100 rounded-lg">
           <thead className="border-b border-gray-500 ">
             <tr className="text-left font-Playfair">
               <th className="flex items-center gap-2 p-4">
                 <input type="checkbox" />
-                <span>Select All</span>
+                 <span>Select All</span>
               </th>
               <th className="p-4">Variation</th>
               <th className="p-4">Unit Price</th>
@@ -41,7 +72,7 @@ const Cart: React.FC<MyComponentProps> = ({ closeModal }) => {
               <OrderCard cartItem={cartItem} />
             ))}
           </tbody>
-        </table>
+        </table> */}
       </section>
     </div>
   );
@@ -51,34 +82,62 @@ const OrderCard = ({ cartItem }: any) => {
   const dispatch = useDispatch();
 
   return (
-    <tr className="border-b border-gray-300 font-WorkSans">
-      <td className="flex gap-2 p-4">
+    <TableRow className="flex items-center justify-between w-full">
+      <TableCell className="flex items-center justify-center gap-2">
         <input type="checkbox" />
-
-        <Image
-          src={cartItem.images}
-          width={100}
-          height={100}
-          alt="A picture of a shoe"
-        />
-        <div className="max-w-[8rem] font-semibold">
-          <span>{cartItem.name}</span>
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src={cartItem.images}
+            width={100}
+            height={100}
+            alt="A picture of a shoe"
+          />
+          <div className="max-w-[8rem] font-semibold">
+            <span>{cartItem.name}</span>
+          </div>
         </div>
-      </td>
-      <td className="p-4">2359c, 20 inches</td>
-      <td className="p-4">{cartItem.price}</td>
-      <td className="p-4">
-        <div>
-          <input className="w-[3rem]" type="number" defaultValue={1} />
-        </div>
-      </td>
-      <td className="p-4">{cartItem.price}</td>
-      <td className="p-4 text-4xl cursor-pointer">
+      </TableCell>
+      <TableCell>2359c, 20 inches</TableCell>
+      <TableCell>{cartItem.price}</TableCell>
+      <TableCell>
+        <input className="w-[3rem]" type="number" defaultValue={1} />
+      </TableCell>
+      <TableCell>{cartItem.price}</TableCell>
+      <TableCell>
         <button onClick={() => dispatch(removeToCart(cartItem.id))}>
           <AiOutlineDelete />
         </button>
-      </td>
-    </tr>
+      </TableCell>
+    </TableRow>
+
+    // <tr className="border-b border-gray-300 font-WorkSans">
+    //   <td className="flex gap-2 p-4">
+    //     <input type="checkbox" />
+
+    //     <Image
+    //       src={cartItem.images}
+    //       width={100}
+    //       height={100}
+    //       alt="A picture of a shoe"
+    //     />
+    //     <div className="max-w-[8rem] font-semibold">
+    //       <span>{cartItem.name}</span>
+    //     </div>
+    //   </td>
+    //   <td className="p-4">2359c, 20 inches</td>
+    //   <td className="p-4">{cartItem.price}</td>
+    //   <td className="p-4">
+    //     <div>
+    //       <input className="w-[3rem]" type="number" defaultValue={1} />
+    //     </div>
+    //   </td>
+    //   <td className="p-4">{cartItem.price}</td>
+    //   <td className="p-4 text-4xl cursor-pointer">
+    //     <button onClick={() => dispatch(removeToCart(cartItem.id))}>
+    //       <AiOutlineDelete />
+    //     </button>
+    //   </td>
+    // </tr>
   );
 };
 
